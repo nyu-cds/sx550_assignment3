a0e5580b9af5be1c98439e0e7ea05a570926a0af
Siyuan Xiang <sx550@nyu.edu>
2017-02-25 21:33:54 -0500

    Added some exceptions

--- binary.py
+++ binary.py
@@ -15,10 +15,27 @@ def zbits(n, k):
     Return:
         result - a set of all such binary strings
     '''
-    # Return empty set if input is invalid
-    if k > n or n <= 0:
-        return set()
+    # Raise exception if k or n is not an int or even not a number
+    try:
+        # If k or n is not an int
+        if int(k) != k or int(n) != n:
+            raise ValueError("k and n must be int")
+    except:
+        # If k or n can not be converted to int
+        raise ValueError("k and n must be int")
     else:
+        # Raise exception if n<=0
+        if n <= 0:
+            raise ValueError("n must be greater than 0")
+
+        # Raise exception if k<0
+        if k < 0:
+            raise ValueError("k must be greater than or equal to 0")
+
+        # Raise exception if k>n
+        if k > n:
+            raise ValueError("k must be less than or equal to n")
+
         # Initialize the result set
         result = set()
 
@@ -28,7 +45,7 @@ def zbits(n, k):
         # Use itertools.combinations to generate all combinations of indices of '0' and let others be '1'
         for indices_of_zeros in combinations(all_indices, k):
             # Generate a list of '0' and '1' according to indices_of_zeros
-            # Use ''.join() to convert it into a string and add to the set
+            # Use ''.join() to convert it into a string
             result.add(''.join(['0' if x in indices_of_zeros else '1' for x in all_indices]))
 
         return result
@@ -54,11 +71,6 @@ def main():
     assert zbits(4, 3) == {'0100', '0001', '0010', '1000'}
     assert zbits(4, 1) == {'0111', '1011', '1101', '1110'}
     assert zbits(5, 4) == {'00001', '00100', '01000', '10000', '00010'}
-
-    # More test cases for invalid input
-    assert zbits(3, 4) == set()
-    assert zbits(-1, -2) == set()
-    assert zbits(-1, 0) == set()
 
 if __name__ == '__main__':
     main()
