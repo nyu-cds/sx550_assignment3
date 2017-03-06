1bd9d3d75942dcb41c77010a74eee920e9871d98
Siyuan Xiang <sx550@nyu.edu>
2017-03-05 21:15:22 -0500

    Updated calculator.py

--- calculator.py
+++ calculator.py
@@ -10,12 +10,35 @@ import numpy as np
 
 # For-loops are slow for matrix calculation
 # Use build-in operators of numpy to speed up calculation
+def add(x,y):
+    """
+    Add two arrays using a Python loop.
+    x and y must be two-dimensional arrays of the same shape.
+    """
+    return np.add(x,y)
+
+
+def multiply(x,y):
+    """
+    Multiply two arrays using a Python loop.
+    x and y must be two-dimensional arrays of the same shape.
+    """
+    return np.multiply(x,y)
+
+
+def sqrt(x):
+    """
+    Take the square root of the elements of an arrays using a Python loop.
+    """
+    return np.sqrt(x)
+
+
 def hypotenuse(x,y):
     """
     Return sqrt(x**2 + y**2) for two arrays, a and b.
     x and y must be two-dimensional arrays of the same shape.
     """
-    xx = np.multiply(x,x)
-    yy = np.multiply(y,y)
-    zz = np.add(xx, yy)
-    return np.sqrt(zz)
+    xx = multiply(x,x)
+    yy = multiply(y,y)
+    zz = add(xx, yy)
+    return sqrt(zz)
